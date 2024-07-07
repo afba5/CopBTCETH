176607a111e3fabdc6840150f9685d2bd1504497
afba5 <alex.fabian.baez.aviles@student.hu-berlin.de>
2024-07-07 20:39:33 +0200

    New files

---
+++ Dist_BTC_ret.R
@@ -0,0 +1,16 @@
+# Distribution of Bitcoin (BTC) log-returns
+
+library(quantmod)
+library(qrmtools)
+df_BTC <- getSymbols('BTC-EUR',src='yahoo',auto.assign=FALSE, from = "2017-11-11",
+                     to = "2024-05-10")
+df_ETH <- getSymbols('ETH-EUR',src='yahoo',auto.assign=FALSE, from = "2017-11-11",
+                     to = "2024-05-10")
+
+BTC_ETH <- cbind(df_BTC$`BTC-EUR.Adjusted`,df_ETH$`ETH-EUR.Adjusted`)
+colnames(BTC_ETH) <- c("BTC-EUR","ETH-EUR") 
+
+R_BTCETH <- returns(BTC_ETH) # calculate log-returns
+
+# Plot histogram of log-returns 
+hist(R_BTCETH$`BTC-EUR`, main = "Histogram of BTC returns", xlab = "", col = "blue3")
---
+++ Dist_ETH_ret.R
@@ -0,0 +1,16 @@
+# Distribution of Ethereum (ETH) log-returns
+
+library(quantmod)
+library(qrmtools)
+df_BTC <- getSymbols('BTC-EUR',src='yahoo',auto.assign=FALSE, from = "2017-11-11",
+                     to = "2024-05-10")
+df_ETH <- getSymbols('ETH-EUR',src='yahoo',auto.assign=FALSE, from = "2017-11-11",
+                     to = "2024-05-10")
+
+BTC_ETH <- cbind(df_BTC$`BTC-EUR.Adjusted`,df_ETH$`ETH-EUR.Adjusted`)
+colnames(BTC_ETH) <- c("BTC-EUR","ETH-EUR") 
+
+R_BTCETH <- returns(BTC_ETH) # calculate log-returns
+
+# Plot histogram of log-returns 
+hist(R_BTCETH$`ETH-EUR`, main = "Histogram of ETH returns", xlab = "", col = "red3")
